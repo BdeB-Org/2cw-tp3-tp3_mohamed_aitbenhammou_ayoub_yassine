d59af1035472ed61ec38337a3357c02a24829c5d
imadait77 <6238360@bdeb.qc.ca>
2024-05-24 15:52:18 -0400

    Ajout de commentaires dans les pages pour la lisiilité.

--- Site_Web/javascript/script.js
+++ Site_Web/javascript/script.js
@@ -1,14 +1,16 @@
 const urlTableProduit = "http://localhost:8080/ords/TP3/produit/";
 
+// Créer un élément html passé en paramètre.
 function createNode(element) {
     return document.createElement(element);
 }
 
+// 
 function append(parent, el) {
     return parent.appendChild(el);
 }
 
-/* Afficher tous les produits de la table "produit". */
+/* Afficher tous les produits de la table "produit" dynamiquement dans la page "produit.html". */
 const listeProduits = document.getElementById("produit");
 fetch(urlTableProduit)
     .then((resp) => resp.json())
