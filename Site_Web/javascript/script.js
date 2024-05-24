2f9da79c75a95187ec65c7ab2d0b3ea0c0a6ed5a
imadait77 <6238360@bdeb.qc.ca>
2024-05-24 16:13:57 -0400

    Création de la méthode createNode() pour pouvoir créer des éléments dynamiquement.

--- Site_Web/javascript/script.js
+++ Site_Web/javascript/script.js
@@ -1,1 +1,6 @@
 const urlTableProduit = "http://localhost:8080/ords/TP3/produit/";
+
+// Créer un élément html passé en paramètre.
+function createNode(element) {
+    return document.createElement(element);
+}
