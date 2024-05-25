436e1e2d13d47e43ced4870bfe8fc3c834748401
imadait77 <6238360@bdeb.qc.ca>
2024-05-25 01:06:25 -0400

    Ajout du bouton pour mettre le produit dans le panier.

--- Site_Web/javascript/script.js
+++ Site_Web/javascript/script.js
@@ -67,6 +67,15 @@ fetch(urlTableProduit)
             prixProduit.innerHTML = `Prix: ${produit.prix}`;
             append(divInformationProduit, prixProduit);
 
+            // Bouton ajouter au panier
+            let boutonAjouterAuPanier = createNode("button");
+            boutonAjouterAuPanier.className = "bouton-ajouter-au-panier"
+            boutonAjouterAuPanier.innerHTML = "Ajouter au panier";
+            boutonAjouterAuPanier.onclick = function() {
+                alert("Le produit a été ajouté au panier.");
+            }
+            append(divProduit, boutonAjouterAuPanier);
+
         });
     })
     .catch(function (error) {
